40954836fbe2d83a0d4fc826545a703f2033116f
w5-bots-official <86280205+w5-bots-official@users.noreply.github.com>
2021-08-05 17:30:37 +0530

    🙂❤

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('LUA BOT UYIR', {
+CFonts.say('W5-BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
